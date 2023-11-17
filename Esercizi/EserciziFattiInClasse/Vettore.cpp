5412f002bfea7ffd1bd6bac1dac7bd8f0f4ef58f
Gabriele Di Pietro <137444679+SerpenTaki@users.noreply.github.com>
2023-11-17 16:55:59 +0100

    Update Vettore.cpp

--- Esercizi/EserciziFattiInClasse/Vettore.cpp
+++ Esercizi/EserciziFattiInClasse/Vettore.cpp
@@ -6,16 +6,47 @@ Deve inoltre includere il costruttore di copia profonda, l'assegnazione profonda
 #include<iostream>
 
 class Vettore{
+    friend Vettore operator+(const Vettore&, const Vettore&); //Le dichiarazioni di amicizia si mettono appena sotto la classe
 private:
     int* a;
     unsigned int size;
     // Vettore vuoto IFF a==nullptr & size==0
+    int* copia() const {
+        int* aux = nullptr;
+        if(size>0){
+            aux = new int(size);
+            for(unsigned int j=0; j<size; ++j) aux[j]=a[j];
+        }
+        return aux;
+    }
 public:
     //Vettore():a(nullptr), size(0) {}
+    // conversione da unsigned int => Vettore
     Vettore(int val=0, unsigned int s=0): a(s==0 ? nullptr : new int[s]), size(s){
         for(unsigned int k=0; k<size; ++k) a[k]=val;
     }
+    Vettore& operator=(const Vettore& v) {
+        if(this != &v){
+            delete[] a; //senza questa invocazione => GARBAGE
+            a = v.copia();
+            size = v.size;
+        }
+        return *this;
+    }
+    Vettore(const Vettore& v): a(v.copia()), size(v.size) {}
+    ~Vettore(){ //Regola del 3 rimane solamente il distruttore
+        delete[] a;
+    }
 };
+
+Vettore operator+(const Vettore& v1, const Vettore& v2){
+    Vettore v(v1.size+v2.size); //non dicendo nulla lui lo inizializza a zero
+    for(unsigned int j=0; j<v.size; ++j){
+        if(j<v1.size) v.a[j]=v1.a[j];
+        else v.a[j] = v2.a[j-v1.size];
+    }
+    return v;
+}
 
 
 int main(){
